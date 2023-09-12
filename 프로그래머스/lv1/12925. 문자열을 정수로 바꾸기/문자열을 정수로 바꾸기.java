0b174bc9d14d27ec467f36dd3a20991a797b1d93
svng-zu <70852514+svng-zu@users.noreply.github.com>
2023-09-12 16:09:52 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        //s는 1~5
+        //s는 0으로 시작 x
+        answer = Integer.valueOf(s);;
+        return answer;
+    }
+}
