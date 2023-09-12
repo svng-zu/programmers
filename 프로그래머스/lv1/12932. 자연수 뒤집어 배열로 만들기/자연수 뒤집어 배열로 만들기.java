bcde3de3188d54c0f34c7af595ca6ba3a491621d
svng-zu <70852514+svng-zu@users.noreply.github.com>
2023-09-12 16:08:48 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(long n) {
+        int[] answer = {};
+        
+        String len = Long.toString(n);
+        int length = len.length();
+        answer = new int[length];
+        for(int i=0; i<length; i++){
+            
+            answer[i] = (int) (n%10);
+            n /= 10;
+        }
+        
+        return answer;
+    }
+}
