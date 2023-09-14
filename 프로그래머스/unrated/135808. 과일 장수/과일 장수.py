e19e3571587ad07a313b966ea11be2e3b7272f45
svng-zu <70852514+svng-zu@users.noreply.github.com>
2023-09-14 18:54:10 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,12 +1,13 @@
 def solution(k, m, score):
     answer = 0
     score.sort()
+    
     n = len(score)
     a = []
     while n >= m :     
         a.append(score[-m])
         for i in range(m) :
-            score.pop()
+            score.pop() #pop()의 시간이 빠르기 때문에 sort를 순서 고려해서 사용
         n -= m
 
         
