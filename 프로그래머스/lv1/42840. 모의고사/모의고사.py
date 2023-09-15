a7a407276838d90288acdea5ae9d953473e43481
svng-zu <70852514+svng-zu@users.noreply.github.com>
2023-09-15 15:51:55 +0900

    [level 1] Title: 모의고사, Time: 4.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,23 +1,24 @@
 def solution(answers):
-    man1 = [1,2,3,4,5]
-    man2 = [2,1,2,3,2,4,2,5]
-    man3 = [3,3,1,1,2,2,4,4,5,5]
+    answer = []
+    list_1 = [1,2,3,4,5] #5를 주기로 반복
+    list_2 = [2,1,2,3,2,4,2,5] 
+    list_3 = [3,3,1,1,2,2,4,4,5,5] #두번씩 반복
     
-    man1 = man1 * ((len(answers)-1) // len(man1) +1)
-    man2 = man2 * ((len(answers)-1) // len(man2) +1)
-    man3 = man3 * ((len(answers)-1) // len(man3) +1)
-    cnt = [0,0,0]
-    for i in range(len(answers)):
-        if answers[i] == man1[i]:
-            cnt[0] += 1
-        if answers[i] == man2[i]:
-            cnt[1] += 1
-        if answers[i] == man3[i]:
-            cnt[2] += 1
+    score = [0, 0, 0]     
+    for i, ans in enumerate(answers) :
+        if ans == list_1[i%5] :
+            score[0] += 1
+        if ans== list_2[i%8] :
+            score[1] += 1
+        if ans == list_3[i%10] :
+            score[2] += 1
+    
+    
+       
+    max_score = max(score)
+    for j,sco in enumerate(score) :
+        if sco == max_score :
+            answer.append(j+1)
+        
             
-    result = []
-    for i in range(len(cnt)):
-        if cnt[i] == max(cnt):
-            result.append(i+1)
-            
-    return result
+    return answer
