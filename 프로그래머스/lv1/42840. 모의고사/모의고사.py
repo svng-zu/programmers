676fbc0402121459f44591b584db35ac4cd04fff
svng-zu <70852514+svng-zu@users.noreply.github.com>
2023-09-15 15:50:05 +0900

    [level 1] Title: 모의고사, Time: 2.60 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -0,0 +1,23 @@
+def solution(answers):
+    man1 = [1,2,3,4,5]
+    man2 = [2,1,2,3,2,4,2,5]
+    man3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    man1 = man1 * ((len(answers)-1) // len(man1) +1)
+    man2 = man2 * ((len(answers)-1) // len(man2) +1)
+    man3 = man3 * ((len(answers)-1) // len(man3) +1)
+    cnt = [0,0,0]
+    for i in range(len(answers)):
+        if answers[i] == man1[i]:
+            cnt[0] += 1
+        if answers[i] == man2[i]:
+            cnt[1] += 1
+        if answers[i] == man3[i]:
+            cnt[2] += 1
+            
+    result = []
+    for i in range(len(cnt)):
+        if cnt[i] == max(cnt):
+            result.append(i+1)
+            
+    return result
