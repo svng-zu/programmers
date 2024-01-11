61db2b9fff4c57ce0b5b4461e07d1dd405266ee3
황성주 <70852514+svng-zu@users.noreply.github.com>
2024-01-12 00:27:48 +0900

    [level Lv.2] Title: 짝지어 제거하기, Time: 197.04 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = -1
+    stack = []
+    for i in range(len(s)) :
+        if len(stack) == 0  :
+            stack.append(s[i])
+        elif stack[-1] == s[i] :
+            stack.pop()
+        else :
+            stack.append(s[i])
+            
+            
+    if len(stack) == 0 :
+        answer = 1
+    else :
+        answer = 0
+            
+
+    return answer
